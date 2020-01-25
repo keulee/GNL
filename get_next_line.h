e1bcd49c7d497054e651602d32b3f9604883036a
keulee <58826218+keulee@users.noreply.github.com>
2020-01-25 18:13:28 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,19 @@
 /*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 20:10:11 by keulee            #+#    #+#             */
-/*   Updated: 2020/01/22 23:06:23 by keulee           ###   ########.fr       */
+/*   Updated: 2020/01/25 16:44:29 by keulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 10
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 
 int		get_next_line(int fd, char **line);
+char	*ft_strdup(const char *src);
+char	*ft_strjoin(const char *s1, const char *s2);
 
 #endif
