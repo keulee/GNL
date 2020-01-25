606a8a6c3fba35821135c19c0f0d46551f1036af
keulee <58826218+keulee@users.noreply.github.com>
2020-01-25 18:15:01 +0100

    Create get_next_line_utils.c

---
+++ get_next_line_utils.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/20 20:09:58 by keulee            #+#    #+#             */
+/*   Updated: 2020/01/25 16:47:36 by keulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+unsigned int	ft_end_of_line(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		return (1);
+	else
+		return (0);
+}
+
+size_t		ft_strlen(const char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char		*ft_strdup(const char *src)
+{
+	char	*copy;
+	int		i;
+	int		len;
+
+	i = 0;
+	copy = NULL;
+	len = ft_strlen(src);
+	if (!(copy = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	while (i < len)
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	unsigned int	new_str_len;
+	unsigned int	i;
+	unsigned int	j;
+	char			*new;
+
+	i = 0;
+	j = 0;
+	new_str_len = ft_strlen(s1) + ft_strlen(s2);
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	if (!(new = (char *)malloc(sizeof(char) * (new_str_len + 1))))
+		return (NULL);
+	while (s1[i] != '\0')
+		new[i++] = (char)s1[j++];
+	j = 0;
+	while (s2[j] != '\0')
+		new[i++] = (char)s2[j++];
+	new[i] = '\0';
+	return (new);
+}
