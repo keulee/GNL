2c99a2320307361a0bdd8d596cca5ed4c1290a75
keulee <58826218+keulee@users.noreply.github.com>
2020-01-31 22:52:15 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 20:10:11 by keulee            #+#    #+#             */
-/*   Updated: 2020/01/30 17:13:07 by keulee           ###   ########.fr       */
+/*   Updated: 2020/01/31 22:44:28 by keulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@
 # include <string.h>
 
 int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *src);
 char	*ft_strjoin(const char *s1, const char *s2);
 int		ft_strchr(const char *s, int c);
 void	ft_bzero(void *s, size_t n);
+char	*ft_substr(const char *str, unsigned int start, size_t len);
 
 #endif
