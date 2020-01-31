98e56867ca88a6203b87bc01b9b2ef150ac55276
keulee <58826218+keulee@users.noreply.github.com>
2020-01-31 22:51:16 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,39 +6,42 @@
 /*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 20:09:16 by keulee            #+#    #+#             */
-/*   Updated: 2020/01/30 19:13:16 by keulee           ###   ########.fr       */
+/*   Updated: 2020/01/31 22:48:29 by keulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int		get_next_line(int fd, char **line)
+char		*ft_new_str(char *str)
+{
+	unsigned int		i;
+
+	i = 0;
+	if (str[i] != '\0' && str[i] != '\n')
+		i++;
+	str = ft_substr(str, i, ft_strlen(str));
+	return (str);
+}
+
+int			get_next_line(int fd, char **line)
 {
 	int		ret;
 	char	buf[BUFFER_SIZE+1];
 	char	*str;
-	int		i;
 
-	i = 1;
 	str = NULL;
-	//bzero(buf, BUFFER_SIZE);
 	if (fd < 0 || line == NULL)
 	{
 		printf("fd Open Error\n");
 		return (-1);
 	}
-	/*if ((ret = read(fd, buf, BUFFER_SIZE)) == 0)
-	{
-		printf("End Of File\n");
-		return (0);
-	}*/
 	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
-		printf("%d try\n", i++);
-		printf("ret = %d\n", ret);
-		printf("buf = %s\n", buf);
+		//printf("str = %s\n", str);
+		//printf("ret = %d\n", ret);
+		//printf("buf = %s\n", buf);
 		if (str == NULL)
 			str = ft_strdup(buf);
 		else
@@ -47,7 +50,11 @@ int		get_next_line(int fd, char **line)
 		if (ft_strchr(buf, '\n') == 1)
 			break;
 	}
-	if (ret == 0)
-		return (0);
+	ft_new_str(str);
 	return (1);
+	if (ret == 0)
+	{
+		printf("EOF");
+		return (0);
+	}
 }
