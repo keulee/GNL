074c6879cf2184e1f26148f6314211744519cfba
keulee <58826218+keulee@users.noreply.github.com>
2020-01-31 22:52:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 12:57:34 by keulee            #+#    #+#             */
-/*   Updated: 2020/01/30 19:10:38 by keulee           ###   ########.fr       */
+/*   Updated: 2020/01/31 22:48:31 by keulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ int		main(void)
 	char	*line;
 
 	line = NULL;
-	fd = open("test2.txt", O_RDONLY);
-	get_next_line(fd, &line);
-	printf("main line: %s\n", line);
+	fd = open("test.txt", O_RDONLY);
+	while (fd > 0)
+	{
+		printf("%d\n", get_next_line(fd, &line));
+		printf("main line: %s\n", line);
+	}
 	close(fd);
 	return (0);
 }
