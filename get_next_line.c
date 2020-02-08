4643f0c4271f45e7730494b4c9c0cfe88840ba13
keulee <58826218+keulee@users.noreply.github.com>
2020-02-08 23:05:54 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,56 @@
 /*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 20:09:16 by keulee            #+#    #+#             */
-/*   Updated: 2020/01/31 23:25:57 by keulee           ###   ########.fr       */
+/*   Updated: 2020/02/08 21:57:36 by keulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-char		**ft_line(char *str, char **line)
+char		**ft_line(char *str, char **line, char *tmp)
 {
 	unsigned int	i;
-    unsigned int    j;
-    char            *tmp;
-    char            *newstr;
+	char					*saveline;
 
 	i = 0;
 	while (str[i] != '\0' && str[i] != '\n')
 		i++;
-    j = ft_strlen(&str[i]);
-    tmp = ft_substr(str, i + 1, j);
-    //printf("tmp : %s\n", tmp);
-	newstr = ft_substr(str, 0, i);
-    //printf("newstr: %s\n", newstr);
-    if (*line == NULL)
-        *line = ft_strdup(newstr);
-    else if (str[i] == '\n')
-    {
-        *line = ft_strjoin(tmp, newstr);
-    }
+	saveline = ft_substr(str, 0, i);
+	if (*line == NULL)
+		*line = ft_strdup(saveline);
+	else
+	{
+		*line = ft_strjoin(tmp, saveline);
+		free(saveline);
+		free(tmp);
+	}
 	return (line);
+}
+
+char		*ft_save_the_rest(char *str, char *tmp)
+{
+	unsigned int		i;
+	unsigned int		j;
+
+	i = 0;
+	while (str[i] != '\0' && str[i] != '\n')
+		i++;
+	j = ft_strlen(&str[i]);
+	tmp = ft_substr(str, i + 1, j);
+	return (tmp);
 }
 
 int			get_next_line(int fd, char **line)
 {
-	int		ret;
-	char	buf[BUFFER_SIZE+1];
-	char	*str;
+	int					ret;
+	char				buf[BUFFER_SIZE+1];
+	char				*str;
+	static char *tmp;
+	char				*tmp_line;
 
 	str = NULL;
-	if (fd < 0 || line == NULL)
+	if (fd < 0 || line == NULL || (read(fd, buf, 0)) < 0)
 	{
 		printf("fd Open Error\n");
 		return (-1);
@@ -52,18 +63,19 @@ int			get_next_line(int fd, char **line)
 	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
-		//printf("ret = %s\n", ret);
-		printf("buf = %s\n", buf);
 		if (str == NULL)
 			str = ft_strdup(buf);
 		else
+		{
+			tmp_line = str;
 			str = ft_strjoin(str, buf);
-		//printf("str phrase = %s\n", str);
+			free(tmp_line);
+		}
 		if (ft_strchr(buf, '\n') == 1)
 			break;
 	}
-	//printf("str phrase = %s\n", str);
-	ft_line(str, line);
+	line = ft_line(str, line, tmp);
+	tmp = ft_save_the_rest(str, tmp);
 	return (1);
 	if (ret == 0)
 	{
