665aeaac9bafa21809e162b4a248163bfaed3ddd
keulee <58826218+keulee@users.noreply.github.com>
2020-02-24 17:13:58 +0100

    main.c finished

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 12:57:34 by keulee            #+#    #+#             */
-/*   Updated: 2020/02/16 04:03:47 by keulee           ###   ########.fr       */
+/*   Updated: 2020/02/24 16:41:33 by keulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,21 @@ int		main(void)
 	int		i;
 
 	i = 0;
-	fd = open("all_the_good_girls_go_to_hell.txt", O_RDONLY);
+	fd = open("test1.txt", O_RDONLY);
 	while ((ret = get_next_line(fd, &line)) > 0)
 	{
 		printf("*%d time | ret : %d | line : %s\n", i++, ret, line);
+		free(line);
 	}
-	printf("ret : %d | line : %s\n", ret, line);
+	if (ret == -1)
+		printf("open error");
+	if (ret == 0)
+	{
+		printf("ret : %d | line : %s\n", ret, line);
+		free(line);
+	}
 	close(fd);
+	//while (1)
+	//	;
 	return (0);
 }
